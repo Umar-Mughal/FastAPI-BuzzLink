1d55e3c8a9a2b980d6987daddd7b6e1a40bbe0a3
Umar Farooq <umar.wzbd@gmail.com>
2024-04-25 18:10:38 +0500

    added black, and also added all posts and create post method

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 
 @app.get("/")
 def home():
-    return {
-        "success": True,
-        "message": "home",
-        "data": {}
-    }
+    return {"success": True, "message": "home", "data": {}}
 
 
 @app.get("/posts")
-def all_posts():
-    return {
-        "success": True,
-        "message": "all_posts",
-        "data": {}
-    }
+def get_posts():
+    return {"success": True, "message": "all posts", "data": {}}
+
+
+@app.post("/posts")
+def create_post(payload: dict = Body):
+    print(payload)
+    return {"success": True, "message": "post created", "data": {}}
