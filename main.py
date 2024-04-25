e6dea164abe999b881d53d4093382b03f6baf16f
Umar Farooq <umar.wzbd@gmail.com>
2024-04-25 13:55:19 +0500

    s

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-# @app.get("/")
-# async def root():
-#     return {"message": "hello world"}
-
-
 @app.get("/")
-def hello_world():
-    return "hello wod"
+async def root():
+    return {"message": "hello world"}
