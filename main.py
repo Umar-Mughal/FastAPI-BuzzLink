9f5176e6d4e7dc2bc8a63f1d0e9625b16f024108
Umar Farooq <umar.wzbd@gmail.com>
2024-04-25 13:21:51 +0500

    automatically restart the server on change (--reload flag)
    - run on terminal: uvicorn main:app --reload

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "hello world"}
+# @app.get("/")
+# async def root():
+#     return {"message": "hello world"}
 
-# starting a dev server
+
+@app.get("/")
+def hello_world():
+    return "hello wod"
